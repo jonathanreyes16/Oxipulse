39d1bef14910ee225d8643bc3763fcbea84d1a2f
jonathanreyes16 <54415853+jonathanreyes16@users.noreply.github.com>
2021-03-26 12:42:06 -0700

    register and login working

--- app/src/main/java/com/example/oxipulse/MainActivity.java
+++ app/src/main/java/com/example/oxipulse/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.oxipulse;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -16,15 +22,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_history, R.id.navigation_check)
+                R.id.navigation_home, R.id.navigation_history, R.id.navigation_check,R.id.navigation_exit)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+        /*
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()==R.id.navigation_exit){
+                    FirebaseAuth auth = FirebaseAuth.getInstance();
+                    auth.signOut();
+                    Intent intent = new Intent(MainActivity.this,StartActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                return false;
+            }
+        });
+        */
+
+
+
     }
 
 }
---
+++ app/src/main/java/com/example/oxipulse/ui/exit/ExitViewModel.java
@@ -0,0 +1,16 @@
+package com.example.oxipulse.ui.exit;
+
+import android.widget.Button;
+
+import androidx.lifecycle.ViewModel;
+
+public class ExitViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+
+    Button btn_exit;
+
+    public void setBtn_exit(Button btn_exit) {
+        this.btn_exit = btn_exit;
+    }
+
+}
