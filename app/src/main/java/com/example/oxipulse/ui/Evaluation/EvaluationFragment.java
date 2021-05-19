309689c06a8c215b2677e97283e5f89fa7cb39ff
Jona <jonathan.reyes16@tectiuna.edu.mx>
2021-05-19 07:29:45 -0700

    layout of EvaluationFragment only

--- app/src/main/java/com/example/oxipulse/ui/Evaluation/EvaluationFragment.java
+++ app/src/main/java/com/example/oxipulse/ui/Evaluation/EvaluationFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,18 +14,21 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.oxipulse.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class EvaluationFragment extends Fragment {
 
     private EvaluationViewModel evaluationViewModel;
+    //declaracion de variables
+    TextInputEditText et_oxigenSat,et_heartRate,et_csv;
+    Button btn_eval;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         evaluationViewModel =
                 new ViewModelProvider(this).get(EvaluationViewModel.class);
         View root = inflater.inflate(R.layout.fragment_evaluation, container, false);
-
-
+        
 
         return root;
     }
